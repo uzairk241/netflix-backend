a9dff1375456901ef125bb78b5f7d5fbd6255400
uzairk241 <m.uzairkhan241@gmail.com>
2023-10-02 00:09:55 +0500

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb+srv://carlover031200:carlover031200@cluster0.0whojid.mongodb.net/NetflixClone", {
+  .connect("ng", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
